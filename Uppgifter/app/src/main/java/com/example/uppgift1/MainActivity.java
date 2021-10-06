d77aa7680313d27c8c9abd299f53634ee0758079
Zulicus <70630780+Zulicus@users.noreply.github.com>
2021-10-06 12:58:33 +0200

    Done

--- Uppgifter/app/src/main/java/com/example/uppgift1/MainActivity.java
+++ Uppgifter/app/src/main/java/com/example/uppgift1/MainActivity.java
@@ -9,13 +9,12 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Globals
     ImageView galaxyImage;
     TextView posText;
     SensorManager sensorManager;
@@ -24,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Variables
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         galaxyImage = (ImageView) findViewById(R.id.galaxyImg);
-
+        //Event Listner
         SensorEventListener sensorEventListener = new SensorEventListener() {
-            @Override public void onAccuracyChanged(Sensor sensor, int i) {}
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
                 if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -38,42 +40,43 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
-
     }
 
+    //Actual Code
     private void getAccelerometer(SensorEvent sensorEvent) {
         posText = (TextView) findViewById(R.id.posString);
+        //Reads the X and Y coords
         float[] values = sensorEvent.values;
         float X_Axis = values[0];
         float Y_Axis = values[1];
-        float Z_Axis = values[2];
+        //Calculates the angle
         double angle = Math.atan2(X_Axis, Y_Axis) / (Math.PI / 180);
-        posText.setText("Angle: " + String.valueOf((int) angle) + "°");
         galaxyImage.setRotation((float) angle);
+        posText.setText("Angle: " + String.valueOf((int) angle) + "°");
+        //Gets the Width and Height of the screen
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         int width = size.x;
         int height = size.y;
-        if(width<X_Axis*-100*2||-width>X_Axis*-100*2){
-            if(width<X_Axis*-100*2){
-                galaxyImage.setX(width/2);
-            }else if (-width>X_Axis*-100*2) {
-                galaxyImage.setX(-width/2);
+        //Checks for out of bounds
+        if (width < X_Axis * -100 * 2 || -width > X_Axis * -100 * 2) {
+            if (width < X_Axis * -100 * 2) {
+                galaxyImage.setX(width / 2);
+            } else if (-width > X_Axis * -100 * 2) {
+                galaxyImage.setX(-width / 2);
             }
-        }else{
-        galaxyImage.setX(X_Axis*-100);
+        } else {
+            galaxyImage.setX(X_Axis * -100);
         }
-        if(height<=Y_Axis*-100*4||-height>Y_Axis*-100*2){
-            if(height<=Y_Axis*-100*4){
-                galaxyImage.setY(-height/4);
-            }else if(-height>Y_Axis*-100*2){
-                galaxyImage.setY(height/2);
+        if (height <= Y_Axis * -100 * 4 || -height > Y_Axis * -100 * 2) {
+            if (height <= Y_Axis * -100 * 4) {
+                galaxyImage.setY(-height / 4);
+            } else if (-height > Y_Axis * -100 * 2) {
+                galaxyImage.setY(height / 2);
             }
-        }else{
-        galaxyImage.setY(Y_Axis*100);
+        } else {
+            galaxyImage.setY(Y_Axis * 100);
         }
-
     }
-
 }
